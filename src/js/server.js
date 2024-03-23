a040aacfb0db487aaad0f578b3cb41ef63ce1d67
Taylor Ann McLean <88504985+taymcl@users.noreply.github.com>
2024-03-22 21:43:40 -0400

    scripts and server fixes

--- src/js/server.js
+++ src/js/server.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors);
+app.use(cors());
 app.use(bodyParser.json());
 
 const transporter = nodemailer.createTransport({
